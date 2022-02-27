779cbaeedfc7c541aee9742d68aaa1a8c161207e
Amoh Prince <81857018+AmohPrince@users.noreply.github.com>
2022-02-27 15:26:29 +0300

    Made it responsive. Made my headache but atleast it works.

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import images from "../../constants/images";
 import "./Navbar.css";
+import { GiHamburgerMenu, GiZeusSword } from "react-icons/gi";
 
 const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
   return (
     <nav className="App__bg App__navbar">
       <div className="nikelogo">
@@ -25,6 +27,43 @@ const Navbar = () => {
           <a href="#Collections"> Collections </a>
         </li>
       </ul>
+
+      <div className="app_navbar_smallscreen">
+        <GiHamburgerMenu
+          className="hambugermenu"
+          color="#000000"
+          fontSize={27}
+          onClick={() => {
+            setToggleMenu(true);
+          }}
+        />
+        {toggleMenu && (
+          <div className="app_navbar_smallscreen_overlay flex_center">
+            <GiZeusSword
+              color="#FFFFFF"
+              className="overlay_close"
+              fontSize={27}
+              onClick={() => {
+                setToggleMenu(false);
+              }}
+            />
+            <ul className="app_navbar_links_smallscreen">
+              <li className="p__inter links_smallscreen ">
+                <a href="#women">Women</a>
+              </li>
+              <li className="p__inter links_smallscreen">
+                <a href="#Men"> Men </a>
+              </li>
+              <li className="p__inter links_smallscreen">
+                <a href="#Kids"> Kids </a>
+              </li>
+              <li className="p__inter links_smallscreen">
+                <a href="#Collections"> Collections </a>
+              </li>
+            </ul>
+          </div>
+        )}
+      </div>
     </nav>
   );
 };
